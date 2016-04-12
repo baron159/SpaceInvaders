699c23c33890d6030fa6f402d6cc8bf2617fbe84
Scott Baron <baron159@live.com>
2016-04-12 01:08:18 -0400

    Basic Game is done.  Looking to make some minor improvements

--- app/src/main/java/com/learing/spaceinvatorscodeschool/DefenceBrick.java
+++ app/src/main/java/com/learing/spaceinvatorscodeschool/DefenceBrick.java
@@ -1,8 +1,45 @@
 package com.learing.spaceinvatorscodeschool;
 
+import android.graphics.RectF;
+
 /**
  * Game Object for the Defence Brick
  * Created by scottbaron on 3/22/16.
  */
 public class DefenceBrick {
+    private RectF rect;
+
+    private boolean isVisible;
+
+    public DefenceBrick(int row, int column, int shelterNumber, int screenX, int screenY){
+
+        int width = screenX / 90;
+        int height = screenY / 40;
+
+        isVisible = true;
+
+        // Sometimes a bullet slips through this padding.
+        // Set padding to zero if this annoys you
+        int brickPadding = 1;
+        // The number of shelters
+        int shelterPadding = screenX / 9;
+        int startHeight = screenY - (screenY /8 * 2);
+
+        rect = new RectF(column * width + brickPadding + (shelterPadding * shelterNumber) + shelterPadding + shelterPadding * shelterNumber,
+                row * height + brickPadding + startHeight,
+                column * width + width - brickPadding + (shelterPadding * shelterNumber) + shelterPadding + shelterPadding * shelterNumber,
+                row * height + height - brickPadding + startHeight);
+    }
+
+    public RectF getRect(){
+        return this.rect;
+    }
+
+    public void setInvisible(){
+        isVisible = false;
+    }
+
+    public boolean getVisibility(){
+        return isVisible;
+    }
 }
--- app/src/main/java/com/learing/spaceinvatorscodeschool/Invader.java
+++ app/src/main/java/com/learing/spaceinvatorscodeschool/Invader.java
@@ -1,8 +1,144 @@
 package com.learing.spaceinvatorscodeschool;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.RectF;
+
+import java.util.Random;
+
 /**
  * Game Object for the Invader
  * Created by scottbaron on 3/22/16.
  */
 public class Invader {
+    RectF rect;
+    Random generator = new Random();
+
+    private Bitmap bitmap1;
+    private Bitmap bitmap2;
+
+    private float length;
+    private float height;
+
+    private float x;
+    private float y;
+
+    private float shipSpeed;
+
+    public final int LEFT = 1;
+    public final int RIGHT = 2;
+
+    private int shipMoving = RIGHT;
+
+    boolean isVisible;
+
+    public Invader(Context context, int row, int column, int screenX, int screenY){
+        rect = new RectF();
+
+        length = screenX / 20;
+        height = screenY / 20;
+
+        isVisible = true;
+
+        int padding = screenX / 25;
+
+        x = column * (length + padding);
+        y = row * (length + padding / 4);
+
+        bitmap1 = BitmapFactory.decodeResource(context.getResources(), R.drawable.invader1);
+        bitmap2 = BitmapFactory.decodeResource(context.getResources(), R.drawable.invader2);
+
+        bitmap1 = Bitmap.createScaledBitmap(bitmap1,
+                (int)(length),
+                (int)(height),
+                false);
+        bitmap2 = Bitmap.createScaledBitmap(bitmap2,
+                (int) (length),
+                (int) (height),
+                false);
+
+        shipSpeed = 40;
+    }
+
+    public void setInvisible(){
+        isVisible = false;
+    }
+
+    public boolean getVisibility(){
+        return isVisible;
+    }
+
+    public RectF getRect(){
+        return rect;
+    }
+
+    public Bitmap getBitmap1(){
+        return this.bitmap1;
+    }
+
+    public Bitmap getBitmap2(){
+        return this.bitmap2;
+    }
+
+    public float getX(){
+        return x;
+    }
+
+    public float getY(){
+        return y;
+    }
+
+    public float getLength(){
+        return length;
+    }
+
+    public void update(long fps){
+        if(shipMoving == LEFT){
+            x = x - shipSpeed / fps;
+        }
+        if (shipMoving == RIGHT){
+            x = x + shipSpeed / fps;
+        }
+
+        rect.top = y;
+        rect.bottom = y + height;
+        rect.left = x;
+        rect.right = x + length;
+    }
+
+    public void dropDownAndReverse(){
+        if(shipMoving == LEFT)
+            shipMoving = RIGHT;
+        else
+            shipMoving = LEFT;
+        y = y + height;
+        shipSpeed = shipSpeed * 1.18f;
+    }
+
+    public boolean takeAim(float playerShipX, float playerShipLength){
+
+        int randomNumber = -1;
+
+        // If near the player
+        if((playerShipX + playerShipLength > x && playerShipX + playerShipLength < x + length) ||
+                (playerShipX > x && playerShipX < x + length)) {
+
+            // A 1 in 500 chance to shoot
+            randomNumber = generator.nextInt(150);
+            if(randomNumber == 0) {
+                return true;
+            }
+
+        }
+
+        // If firing randomly (not near the player) a 1 in 5000 chance
+        randomNumber = generator.nextInt(2000);
+        if(randomNumber == 0){
+            return true;
+        }
+
+        return false;
+    }
+
 }
